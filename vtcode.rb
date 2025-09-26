49c3ef5c1fe5ba29e33d5a904adc71097c523522
Vinh Nguyen <vinhnguyen2308@gmail.com>
2025-09-26 21:47:42 +0700

    Update vtcode formula to v0.15.0

--- vtcode.rb
+++ vtcode.rb
@@ -1,15 +1,15 @@
 class Vtcode < Formula
   desc "A Rust-based terminal coding agent with modular architecture"
   homepage "https://github.com/vinhnx/vtcode"
-  version "0.8.2"
+  version "0.15.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vinhnx/vtcode/releases/download/v#{version}/vtcode-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "0d70b96b771634d1d61cff9cd18d3d7b26090202ae710c58d8cfcc189d4dd61d"
+      sha256 "be36d8c62dded1dfd33874de310c1dca7f4ffeaa7c778d6e82ec8e1e75af50ea"
     else
       url "https://github.com/vinhnx/vtcode/releases/download/v#{version}/vtcode-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "CHANGE_THIS_SHA256_AFTER_FIRST_RELEASE" # Calculate: shasum -a 256 vtcode-v0.8.2-x86_64-apple-darwin.tar.gz
+      sha256 "584210b9c533ab55977e9cb17edc76ebb196aeb7aef4fcc714cc6301b475ae65"
     end
   end
 
